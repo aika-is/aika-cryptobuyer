538dbeb151c30c54897a7488387e35e7b4470bbc
colorrin <mb.andres@gmail.com>
2021-05-05 23:40:24 -0400

    Symbol picker

--- app/helpers/binance_helper.rb
+++ app/helpers/binance_helper.rb
@@ -143,6 +143,7 @@ module BinanceHelper
 			state = SymbolState.where(good: true, symbol_name: {'$nin': not_in}).sort(matches: -1).first
 			price = prices.find{|e| e[:symbol] == state.symbol_name}[:price].to_f
 			if state.goal > price
+				not_in << state.symbol_name
 				state = nil
 			end
 		end
