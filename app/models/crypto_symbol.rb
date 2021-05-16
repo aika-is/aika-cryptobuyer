c4d93e13291489dbf2a51c5a9bd157fda85216cd
colorrin <mb.andres@gmail.com>
2021-05-16 19:01:39 -0400

    Symbol delisting

--- app/models/crypto_symbol.rb
+++ app/models/crypto_symbol.rb
@@ -15,7 +15,7 @@ class CryptoSymbol
 		CryptoSymbol.where(client_ids: client_id)
 	end
 
-	def deregister_not_in_symbols!(client_id, symbols)
+	def self.deregister_not_in_symbols!(client_id, symbols)
 		CryptoSymbol.where(symbol_name: {'$nin': symbols}, client_ids: client_id).update_all({'$pull': {client_ids: 'client_id'}})
 	end
 end
