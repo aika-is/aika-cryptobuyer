4de78b48e37da4fcf41f40cebcd9dca5aadb24e5
colorrin <mb.andres@gmail.com>
2021-05-16 16:07:06 -0400

    Wallet - Prevented Infinite loops on setter

--- app/helpers/encryption_helper.rb
+++ app/helpers/encryption_helper.rb
@@ -12,7 +12,7 @@ module EncryptionHelper
 
 	def decrypt_text(inner_key, encoded)
 		cipher = OpenSSL::Cipher.new('DES-EDE3-CBC').decrypt
-		cipher.key = "#{ENV['ENC_KEY']}_#{inner_key}"
+		cipher.key = "#{ENV['ENC_KEY']}#{inner_key}"
 		s = [encoded].pack("H*").unpack("C*").pack("c*")
 
 		text = cipher.update(s) + cipher.final
