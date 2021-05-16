682fd9a084ec676d41854ae53039c2575ea0ab31
colorrin <mb.andres@gmail.com>
2021-05-16 17:50:19 -0400

    Update Indicator Fixes

--- app/misc/indicators/last_price_one_minute.rb
+++ app/misc/indicators/last_price_one_minute.rb
@@ -12,11 +12,11 @@ module Indicators
 			puts "FETCHING #{symbol_name} - #{self.indicator_id} - #{time}"
 			i = 0
 			trades = []
-			while(trades.length == 0) do
+			while trades.length == 0
 				from = Time.at((time.to_i / self.interval)*self.interval)-(i*self.interval)
 				to = from + self.interval
-				trades = Wallet.client_for(client_id).get_trades symbol_name, from, to
-				i++
+				trades = Wallet.client_for(client_id).get_trades(symbol_name, from, to)
+				i += 1
 			end
 			price = trades.last[:price]
 			SymbolIndicator.create!(client_id: client_id, symbol_name: symbol_name, indicator_id: self.indicator_id, interval: self.interval, interval_time: from, value: price)
