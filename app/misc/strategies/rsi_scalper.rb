a8899bf5a1d96480d3bd78e1066c3351e53e288f
colorrin <mb.andres@gmail.com>
2021-05-17 02:26:59 -0400

    Complete Wallet Worker

--- app/misc/strategies/rsi_scalper.rb
+++ app/misc/strategies/rsi_scalper.rb
@@ -26,7 +26,7 @@ module Strategies
 
 			if cash > order_amount
 				not_in = wallet.client.get_positioned_assets(wallet).collect{|e| "#{e[:asset]}#{wallet.base_coin}"}
-				symbol_indicator = pick_symbol(not_in)
+				symbol_indicator = pick_symbol(wallet, not_in)
 				
 				price = wallet.client.get_price(symbol_indicator.symbol_name)[:price]
 				order = wallet.client.perform_market_buy(wallet, symbol_indicator.symbol_name, order_amount)
