828dd5d13e46b28d4d4ffc0ab7d06cfeee07e2a9
colorrin <mb.andres@gmail.com>
2021-05-16 23:36:56 -0400

    Better Multithreading using Celluloid

--- app/misc/indicator_worker.rb
+++ app/misc/indicator_worker.rb
@@ -2,6 +2,7 @@ require 'celluloid'
 
 class IndicatorWorker
 	include Celluloid
+
 	def process_symbol_indicator(client_id, symbol_name, indicator_properties, time)
 		puts "STARTING #{symbol_name}"
 		indicator = SymbolIndicator.collect_for(client_id, symbol_name, indicator_properties[:indicator_id], time, indicator_properties[:interval])
