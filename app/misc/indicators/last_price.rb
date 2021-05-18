5d97fe0abccf78501d223c2643e48a9f1abaa50d
colorrin <mb.andres@gmail.com>
2021-05-17 23:32:15 -0400

    Improved Last Price fetch

--- app/misc/indicators/last_price.rb
+++ app/misc/indicators/last_price.rb
@@ -8,24 +8,21 @@ module Indicators
 		def self.fetch_symbol_indicator client_id, symbol_name, time, interval
 			truncated_time = Time.at((time.to_i / interval)*interval)
 			truncated_time = truncated_time - interval if truncated_time + interval > Time.now
-			i = 0
 			price = nil
 			trades = []
-			from = truncated_time-(i*interval)
-			to = from + interval
-			s = SymbolIndicator.find_by(client_id: client_id, symbol_name: symbol_name, indicator_id: self.indicator_id, interval: interval, interval_time: from)
+			s = SymbolIndicator.find_by(client_id: client_id, symbol_name: symbol_name, indicator_id: self.indicator_id, interval: interval, interval_time: truncated_time)
 			if s.nil? || s.value.nil?
+				from = truncated_time
+				to = from + interval
 				trades = Wallet.client_for(client_id).get_trades(symbol_name, from, to)
 				price = trades.last[:price] if trades.length > 0
 				if !price.present?
-					i += 1
-					from = truncated_time-(i*interval)
+					from = truncated_time-(interval)
 					puts "FETCHING #{symbol_name} - #{self.indicator_id} - #{time} - #{from}"
-					#puts "FROM #{symbol_name} - #{from}, #{to}"
 					symbol = self.fetch_symbol_indicator client_id, symbol_name, from, interval
 					price = symbol.value if symbol.present?
 				end
-				s = SymbolIndicator.find_or_create_by!(client_id: client_id, symbol_name: symbol_name, indicator_id: self.indicator_id, interval: interval, interval_time: from)
+				s = SymbolIndicator.find_or_create_by!(client_id: client_id, symbol_name: symbol_name, indicator_id: self.indicator_id, interval: interval, interval_time: truncated_time)
 				s.value = price
 				s.save
 			end
