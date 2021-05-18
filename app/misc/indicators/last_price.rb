a66f7b6cd250bd27676ba690e1a1e360ec4649fa
colorrin <mb.andres@gmail.com>
2021-05-17 22:21:50 -0400

    Improver Last Price retrofetch

--- app/misc/indicators/last_price.rb
+++ app/misc/indicators/last_price.rb
@@ -13,18 +13,21 @@ module Indicators
 			trades = []
 			from = truncated_time-(i*interval)
 			to = from + interval
-			trades = Wallet.client_for(client_id).get_trades(symbol_name, from, to)
-			price = trades.last[:price] if trades.length > 0
-			while !price.present?
-				i += 1
-				from = truncated_time-(i*interval)
-				puts "FETCHING #{symbol_name} - #{self.indicator_id} - #{time} - #{from}"
-				#puts "FROM #{symbol_name} - #{from}, #{to}"
-				symbol = self.fetch_symbol_indicator client_id, symbol_name, from, interval
-				price = symbol.value if symbol.present?
-			end
 			s = SymbolIndicator.find_or_create_by!(client_id: client_id, symbol_name: symbol_name, indicator_id: self.indicator_id, interval: interval, interval_time: from)
-			s.set(value: price)
+			if s.nil? && s.price.present?
+				trades = Wallet.client_for(client_id).get_trades(symbol_name, from, to)
+				price = trades.last[:price] if trades.length > 0
+				while !price.present?
+					i += 1
+					from = truncated_time-(i*interval)
+					puts "FETCHING #{symbol_name} - #{self.indicator_id} - #{time} - #{from}"
+					#puts "FROM #{symbol_name} - #{from}, #{to}"
+					symbol = self.fetch_symbol_indicator client_id, symbol_name, from, interval
+					price = symbol.value if symbol.present?
+				end
+				s = SymbolIndicator.find_or_create_by!(client_id: client_id, symbol_name: symbol_name, indicator_id: self.indicator_id, interval: interval, interval_time: from)
+				s.set(value: price)
+			end
 			s
 		end
 	end
