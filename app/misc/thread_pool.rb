76658c028352827e9ef6c992a5fbd2595129a455
colorrin <mb.andres@gmail.com>
2021-05-18 16:04:55 -0400

    Thread Worker

--- app/misc/thread_pool.rb
+++ app/misc/thread_pool.rb
@@ -4,9 +4,7 @@ class ThreadPool
 		@size = size
 		@threads = []
 		@workers = []
-		@pool_id = rand(999)
 		@restart = restart
-		@evicted = []
 	end
 
 	def append(worker)
@@ -16,20 +14,18 @@ class ThreadPool
 	end
 
 	def check_availability
-		if @workers.length == 0 && @restart
-			puts "RESTARTING"
-			@workers = @workers + @evicted
-		end
 		@threads = @threads.delete_if{|e| !e.alive?}
 		if @threads.length-1 < @size && @workers.length > 0
 			worker = @workers.shift
-			@evicted << worker
 			@threads << Thread.new(worker) {|worker|
 				if worker.present?
 					worker.perform
 				end
 				check_availability
 			}
+			if @restart && worker.present?
+				@workers << worker
+			end
 		end
 	end
 end
